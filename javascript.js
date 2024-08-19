b6cce11d05eefbe1c6031f800ca0cce01fda83ff
kreeshmask23 <166102008+kreeshmask23@users.noreply.github.com>
2024-08-18 21:13:32 -0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,14 @@
+function updateClock() {
+    const clockElement = document.getElementById('clock');
+    const now = new Date();
+
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    clockElement.textContent = `${hours}:${minutes}:${seconds}`;
+}
+setInterval(updateClock, 1000);
+updateClock();
